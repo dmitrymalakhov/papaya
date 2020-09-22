70722641967a3e50f736745a21c564b381423e65
Dmitry Malakhov <dmitrii.malakhov@gmail.com>
2020-09-22 22:30:21 +0300

    Initialize firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const AppStyled = styled.div`
+  padding: 10px;
+`;
 
 export default function App() {
-  return <div className="App"></div>;
+  const [user, setUser] = useState(null);
+
+  const handleClickLogin = () => {
+    window.FB.login(function (response) {
+      if (response.authResponse) {
+        console.log("Welcome!  Fetching your information.... ");
+        window.FB.api("/me", function (response) {
+          console.log("Good to see you, " + response.name + ".");
+          setUser(response);
+        });
+      } else {
+        console.log("User cancelled login or did not fully authorize.");
+      }
+    });
+  };
+
+  return (
+    <AppStyled>
+      {user ? (
+        <div>{user.name}</div>
+      ) : (
+        <button onClick={handleClickLogin}>LOGIN</button>
+      )}
+    </AppStyled>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import firebase from 'firebase';
+
+var config = {
+  apiKey: "apiKey",
+  authDomain: "projectId.firebaseapp.com",
+  databaseURL: "https://databaseName.firebaseio.com",
+  storageBucket: "bucket.appspot.com"
+};
+firebase.initializeApp(config);
 
 const rootElement = document.getElementById("root");
 
